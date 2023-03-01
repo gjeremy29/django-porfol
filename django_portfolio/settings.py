9d44ed8b3a5cdc9385e69796bbe2dfea8eb42b81
gjeremy29 <gjeremy0229@outlook.es>
2023-02-28 23:03:07 -0500

    errorcc

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -34,10 +34,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
-STATICFILES_DIRS = [
-    '/opt/render/project/src/staticfiles/',
-    # otras rutas donde se encuentran tus archivos estáticos
-]
+
 
 
 # Application definition
